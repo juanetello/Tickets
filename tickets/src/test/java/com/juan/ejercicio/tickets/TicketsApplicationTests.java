63768069b03d5380e7a2f61925e8d050d5ce4e3e
Juan Tello <juanetello.54@gmail.com>
2020-02-06 21:13:24 -0300

    Comienzo Spring

---
+++ tickets/src/test/java/com/juan/ejercicio/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.ejercicio.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
